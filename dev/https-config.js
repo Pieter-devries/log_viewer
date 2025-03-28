e54bb9cbbe4ee493b379e6aaf81a75f86b598846
Pieter deVries <pieterdevries@google.com>
2025-03-28 13:53:19 +0900

    first commit

---
+++ dev/https-config.js
@@ -0,0 +1,8 @@
+// https-config.js
+const path = require('path'); // Import Node.js path module
+
+module.exports = {
+    // Use path.join to create absolute paths from the config file's directory
+    cert: path.join(__dirname, "localhost+2.pem"),
+    key: path.join(__dirname, "localhost+2-key.pem"),
+};
