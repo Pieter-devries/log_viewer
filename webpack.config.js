65a879f926b2e93b31dcb4619a8f8274c663a267
Pieter deVries <pieterdevries@google.com>
2025-03-31 13:43:42 +0900

    updated main.ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const httpsConfig = require('./https-config.js'); // Import the config
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
@@ -24,29 +23,6 @@ module.exports = (env, argv) => {
                     exclude: /node_modules/,
                 },
             ],
-        },
-        devServer: {
-            host: '0.0.0.0',
-            server: {
-                type: 'https',
-                options: {
-                    cert: httpsConfig.cert, // Use the certificate from the config
-                    key: httpsConfig.key,   // Use the key from the config
-                },
-            },
-            port: 9000,
-            static: {
-                directory: path.join(__dirname, 'dist'),
-            },
-            compress: true,
-            hot: true,
-            headers: {
-                'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-                'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Accept, Origin', 
-            },
-            // Add this line to allow serving from any host
-            allowedHosts: 'all',
         },
     };
 };
