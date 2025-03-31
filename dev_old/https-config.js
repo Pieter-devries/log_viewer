eda73403fa46543ca744e2f4fe10d041e292d8fd
Pieter deVries <pieterdevries@google.com>
2025-03-31 12:47:50 +0900

    Untrack ignored files (.gitignore)

--- dev_old/https-config.js
+++
@@ -1,8 +0,0 @@
-// https-config.js
-const path = require('path'); // Import Node.js path module
-
-module.exports = {
-    // Use path.join to create absolute paths from the config file's directory
-    cert: path.join(__dirname, "localhost+2.pem"),
-    key: path.join(__dirname, "localhost+2-key.pem"),
-};
